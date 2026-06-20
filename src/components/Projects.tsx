2e6ae3ec25d5c26a84957c012cb28efcce16fc96
abilbiju <abilbiju2004@gmail.com>
2026-06-20 07:53:17 +0530

    "Update on Project"

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,12 @@ import { ArrowUpRight } from "lucide-react";
 
 const projects = [
   {
+    title: "BCI System for Multi-Objective Control with IoT Integration",
+    description: "Developed an EEG-based Brain-Computer Interface (BCI) system that enables hands-free control of multiple IoT devices through real-time neural signal processing and classification.",
+    tags: ["Brain Computing Interface","Deep Learning", "Signal Processing", "Python", "IoT"],
+    link: "#",
+  },
+  {
     title: "Live Event Detection System",
     description: "Real-time audio-based deep learning system to detect events and automatically send alerts upon detection.",
     tags: ["Deep Learning", "Audio Processing", "Python"],
